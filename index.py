c80e7850d5404c65430b75bc14cb699c99bc6244
rocketmarker <rocketmarc9@gmail.com>
2023-04-25 18:41:14 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,85 +1,86 @@
-import tkinter as tk
-from tkinter import messagebox, filedialog
-import requests
-import json
-
 class Notepad:
     def __init__(self, master):
-        print("Initializing Notepad...")
         self.master = master
-        self.master.title("Notepad")
-        
-        self.create_widgets()
+        master.title("Notepad")
 
-    def create_widgets(self):
-        # Create the text editor widget
-        self.text_editor = tk.Text(self.master, font=("Helvetica", 12), undo=True)
-        self.text_editor.pack(expand=True, fill="both")
-        
-        # Create the menu bar
-        self.menu_bar = tk.Menu(self.master)
-        self.master.config(menu=self.menu_bar)
-        
-        # Create the File menu
-        self.file_menu = tk.Menu(self.menu_bar, tearoff=False)
+        # Create Text widget
+        self.text_editor = Text(master, font=("Helvetica", 16))
+        self.text_editor.pack(fill=BOTH, expand=True)
+
+        # Create Menu bar
+        self.menu_bar = Menu(master)
+        master.config(menu=self.menu_bar)
+
+        # Add File menu
+        self.file_menu = Menu(self.menu_bar, tearoff=0)
         self.file_menu.add_command(label="New", command=self.new_file)
         self.file_menu.add_command(label="Open", command=self.open_file)
         self.file_menu.add_command(label="Save", command=self.save_file)
-        self.file_menu.add_command(label="Save As", command=self.save_file_as)
         self.file_menu.add_separator()
-        self.file_menu.add_command(label="Exit", command=self.exit_application)
+        self.file_menu.add_command(label="Exit", command=master.quit)
         self.menu_bar.add_cascade(label="File", menu=self.file_menu)
-        
-        # Create the Edit menu
-        self.edit_menu = tk.Menu(self.menu_bar, tearoff=False)
-        self.edit_menu.add_command(label="Undo", command=self.text_editor.edit_undo)
-        self.edit_menu.add_command(label="Redo", command=self.text_editor.edit_redo)
-        self.edit_menu.add_separator()
-        self.edit_menu.add_command(label="Cut", command=self.cut_text)
-        self.edit_menu.add_command(label="Copy", command=self.copy_text)
-        self.edit_menu.add_command(label="Paste", command=self.paste_text)
-        self.edit_menu.add_command(label="Select All", command=self.select_all_text)
-        self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
-        
-        # Create the Tools menu
-        self.tools_menu = tk.Menu(self.menu_bar, tearoff=False)
-        self.tools_menu.add_command(label="Calculator", command=self.open_calculator)
-        self.menu_bar.add_cascade(label="Tools", menu=self.tools_menu)
 
-    # File menu functions
+        # Add Edit menu
+        self.edit_menu = Menu(self.menu_bar, tearoff=0)
+        self.edit_menu.add_command(label="Cut", command=self.cut)
+        self.edit_menu.add_command(label="Copy", command=self.copy)
+        self.edit_menu.add_command(label="Paste", command=self.paste)
+        self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
+
+        # Add Help menu
+        self.help_menu = Menu(self.menu_bar, tearoff=0)
+        self.help_menu.add_command(label="About", command=self.about)
+        self.menu_bar.add_cascade(label="Help", menu=self.help_menu)
+
+        # Add Calculator menu
+        self.calc_menu = Menu(self.menu_bar, tearoff=0)
+        self.calc_menu.add_command(label="Calculate", command=self.calculate)
+        self.menu_bar.add_cascade(label="Calculator", menu=self.calc_menu)
+
     def new_file(self):
-        self.text_editor.delete(1.0, "end")
-        
+        self.text_editor.delete(1.0, END)
+
     def open_file(self):
         file_path = filedialog.askopenfilename()
         if file_path:
             with open(file_path, "r") as file:
-                self.text_editor.delete(1.0, "end")
-                self.text_editor.insert("end", file.read())
+                file_contents = file.read()
+                self.text_editor.delete(1.0, END)
+                self.text_editor.insert(1.0, file_contents)
 
     def save_file(self):
-        if not hasattr(self, "file_path"):
-            self.save_file_as()
-        else:
-            with open(self.file_path, "w") as file:
-                file.write(self.text_editor.get(1.0, "end"))
-
-    def save_file_as(self):
-        file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+        file_path = filedialog.asksaveasfilename()
         if file_path:
-            self.file_path = file_path
-            self.save_file()
+            with open(file_path, "w") as file:
+                file_contents = self.text_editor.get(1.0, END)
+                file.write(file_contents)
 
-    def exit_application(self):
-        if messagebox.askyesno("Exit", "Are you sure you want to exit?"):
-            self.master.destroy()
-
-    # Edit menu functions
-    def cut_text(self):
+    def cut(self):
         self.text_editor.event_generate("<<Cut>>")
 
-    def copy_text(self):
+    def copy(self):
         self.text_editor.event_generate("<<Copy>>")
 
-    def paste_text(self):
+    def paste(self):
         self.text_editor.event_generate("<<Paste>>")
+
+    def about(self):
+        messagebox.showinfo("About", "This is a simple notepad application")
+
+    def calculate(self):
+        calc_window = Toplevel(self.master)
+        calc_window.title("Calculator")
+        calc_window.geometry("200x200")
+
+        entry = Entry(calc_window, width=25)
+        entry.pack(side=TOP, padx=10, pady=10)
+
+        def calculate_result():
+            result = eval(entry.get())
+            label.config(text=f"Result: {result}")
+
+        button = Button(calc_window, text="Calculate", command=calculate_result)
+        button.pack(side=TOP, padx=10, pady=10)
+
+        label = Label(calc_window)
+        label.pack(side=TOP, padx=10, pady=10)
